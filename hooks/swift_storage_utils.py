c4ff348b214c944f88c4a0d130b253d353c2c77c
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-09-26 19:20:43 +0100

    fixed ipv6 support check

--- hooks/swift_storage_utils.py
+++ hooks/swift_storage_utils.py
@@ -224,8 +224,8 @@ def save_script_rc():
     _save_script_rc(**env_vars)
 
 
-def setup_ipv6():
-    ubuntu_rel = float(lsb_release()['DISTRIB_RELEASE'])
-    if ubuntu_rel < 14.04:
-        raise Exception("IPv6 is not supported for Ubuntu "
+def assert_charm_supports_ipv6():
+    """Check whether we are able to support charms ipv6."""
+    if lsb_release()['DISTRIB_CODENAME'].lower() < "trusty":
+        raise Exception("IPv6 is not supported in the charms for Ubuntu "
                         "versions less than Trusty 14.04")
