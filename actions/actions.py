9c7ffa1abac88518e5858e7df2cd41d9b3c6cd78
Adam Collard <adam.collard@canonical.com>
2015-07-17 18:12:04 +0100

    Initial pause action

--- actions/actions.py
+++ actions/actions.py
@@ -5,33 +5,57 @@ import os
 import sys
 import yaml
 
+from charmhelpers.core.host import service_pause
+from charmhelpers.core.hookenv import action_fail
+
+from lib.swift_storage_utils import SWIFT_SVCS
+
 
 def get_action_parser(actions_yaml_path, action_name):
+    """Make an argparse.ArgumentParser seeded from actions.yaml definitions."""
     with open(actions_yaml_path) as fh:
         doc = yaml.load(fh)[action_name]["description"]
-    parser = argparse.ArgumentParser(doc)
+    parser = argparse.ArgumentParser(description=doc)
     # TODO: Add arguments for params defined in the actions.yaml
     return parser
 
 
 def pause(args):
-    from swift_storage_utils import SWIFT_SVCS
-    from charmhelpers.core.host import service_pause
+    """Pause all the swift services."""
+    # TODO: What if some services stop and others don't?
     for service in SWIFT_SVCS:
         service_pause(service)
 
+# A dictionary of all the defined actions to callables (which take
+# parsed arguments).
+ACTIONS = {"pause": pause}
+
 
 def main(argv):
-    cwd, action_name = os.path.split(__file__)
-    actions_yaml_path = os.path.join(cwd, "..", "actions.yaml")
+    action_name = _get_action_name()
+    actions_yaml_path = _get_actions_yaml_path()
     parser = get_action_parser(actions_yaml_path, action_name)
     args = parser.parse_args(argv)
     try:
-        action = globals()[action_name]
-    except AttributeError:
+        action = ACTIONS[action_name]
+    except KeyError:
         return "Action %s undefined" % action_name
     else:
-        return action(args)
+        try:
+            action(args)
+        except Exception as e:
+            action_fail(str(e))
+
+
+def _get_action_name():
+    """Return the name of the action."""
+    return os.path.filename(__file__)
+
+
+def _get_actions_yaml_path():
+    """Return the path to actions.yaml"""
+    cwd = os.path.dirname(__file__)
+    return os.path.join(cwd, "..", "actions.yaml")
 
 
 if __name__ == "__main__":
